4e6c490563906b27303a2f9ef57567d3b123cf65
Jessica <xinyunl419@gmial.com>
2015-10-27 22:26:48 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,27 @@
+####################
+# 1
+
+puts "Tip on a 55 dollar meal: $%.2f" %(55 * 0.25)
+
+
+####################
+# 2
+
+str_and_int = "There are only " + 10.to_s + " type of people in the world, those who understand binary and those who don't."
+puts str_and_int
+
+####################
+# 3
+
+puts "45628 multiplied by 7839 equals %d" %(45628 * 7839)
+
+####################
+# 4
+
+puts (true && false) || (false && true) || !(false && false)
+
+# => false			# => false			# => true
+# => false 			or 	 false 			or   true
+# true
+
+
